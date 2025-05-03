316b0eb79e1475db01ae3792e5566d2a458497ee
Aman <amanguptaa127@gmail.com>
2025-05-03 14:39:03 +0530

    Add refreshAccessToken endpoint

--- src/app.js
+++ src/app.js
@@ -24,8 +24,16 @@ import userRouter from './routes/user.routes.js'
 
 //routes  declaration 
 app.use("/api/v1/users", userRouter )
+// const corsOptions = {
+//     origin: "/api/v1/users", // Adjust as needed
+//     credentials: true
+// }
+// app.use(cors("/api/v1/users"), userRouter);
 
 
 // http://localhost:8000/api/v1/users/register
 
 export default app
+
+
+
